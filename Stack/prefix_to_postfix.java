99fb470cfd4dc98d847f7fb53f506c5249380a08
Vicky Gupta <vickyguptagkp55@gmail.com>
2025-02-24 20:17:55 +0530

    Add implementation for converting prefix expressions to postfix notation using stacks

---
+++ Stack/prefix_to_postfix.java
@@ -0,0 +1,25 @@
+package Stack;
+
+import java.util.Stack;
+
+public class prefix_to_postfix {
+    public static void main(String[] args) {
+        String str = "-9/*+5346";
+        Stack<String> val = new Stack<>();
+
+        for(int i=str.length()-1;i>=0;i--){
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+
+            if(ascii>=48 && ascii<=57){
+                val.push(""+ ch);
+            }
+            else{
+                String v1 = val.pop();
+                String v2 = val.pop();
+                val.push(v1+v2+ch);
+            }
+        }
+        System.out.println(val.pop());
+    }
+}
