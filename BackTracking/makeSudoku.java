861491e28bbd608bc45708e1ff8d046842d88fcf
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-14 10:53:16 +0530

    Solve sudoku

---
+++ BackTracking/makeSudoku.java
@@ -0,0 +1,77 @@
+package BackTracking;
+
+class makeSudoku {
+    static int check = 0;
+    public void solveSudoku(char[][] board) {
+        sudoku(board,0,0);
+        check = 0;
+    }
+
+    public void sudoku(char[][] board, int row, int col){
+        if(row == 9){
+            check = 1;
+            return;
+        }
+
+        else if(board[row][col] != '.'){
+            if(col != 8) sudoku(board,row,col+1);
+            else sudoku(board,row+1,0);
+        }
+        else{
+            for(char i='1';i<='9';i++){
+                if(isValid(board,row,col,i)){
+                    board[row][col] = i;
+                    if(col != 8) sudoku(board,row,col+1);
+                    else sudoku(board,row+1,0);
+                    if(check == 1) return;
+                    board[row][col] = '.';
+                }
+            }
+        }
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char num){
+        // check row
+        for(int j=0;j<9;j++){
+            if(j!=col && board[row][j] == num) return false;
+        }
+        // check column
+        for(int i=0;i<9;i++){
+            if(i != row && board[i][col] == num) return false;
+        }
+        // check matrix
+        int startrow = row/3*3;
+        int startcol = col/3*3;
+        for(int i=startrow;i<startrow+3;i++){
+            for(int j=startcol;j<startcol+3;j++){
+                if(i!=row && j!=col && board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        makeSudoku solver = new makeSudoku();
+        solver.solveSudoku(board);
+
+        // Print the solved board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
