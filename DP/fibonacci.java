2d38a5dc200963efc5930f15324dc31ff02a8588
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-16 21:06:30 +0530

    All fibonacci methods included

--- DP/fibonacci.java
+++ DP/fibonacci.java
@@ -2,7 +2,7 @@ package DP;
 
 public class fibonacci{
     public static void main(String[] args) {
-        int n = 6;
+        int n = 20;
         if(n<0){
             System.out.println("Enter number greater than 0");
             return;
@@ -12,14 +12,14 @@ public class fibonacci{
         for(int i=0;i<=n;i++){
             dp[i] = -1;
         }
-        System.out.println("Fibonacci Sum of "+n+": "+FibCal(n,dp));
+        System.out.println("Memoization fibonacci Sum of "+n+": "+FibCal(n,dp));    // Memoization technique. 
 
         for(int i=0;i<=n;i++){
             dp[i] = 0;
         }
+        System.out.println("Tabulation method fibonacci Sum of "+n+": "+FibCalOpt(n,dp));    // Tabulation method
 
-        System.out.println("Fibonacci Sum of "+n+": "+FibCalOpt(n,dp));
-        // System.out.println("Fibonacci Sum of "+n+": "+Ord_one(n,dp));
+        System.out.println(" Optimized fibonacci Sum of "+n+": "+Ord_one(n));     // Most optimized solution saving whole space complexity.
     }
 
     public static int FibCal(int n, int[] dp){
@@ -30,13 +30,16 @@ public class fibonacci{
 
         if(dp[n] != -1) return dp[n];
 
-        return FibCal(n-1, dp)+FibCal(n-2, dp);
+        dp[n] = FibCal(n-1, dp)+FibCal(n-2, dp);
+        return dp[n];
     }
 
     public static int FibCalOpt(int n, int[] dp){
         // Tabulation method where bottom-up technique is followed.
             // Time complexity: O(N)
             // Space complexity: O(N) ; this is taking only array initialization space.
+        if(n == 0) return 0;
+        
         dp[0] = 0;
         dp[1] = 1;
 
@@ -45,5 +48,22 @@ public class fibonacci{
         }
         
         return dp[n];
+    }
+
+    public static int Ord_one(int n){
+        // this is simple technique or say most optimized one where:
+            // Time complexity: O(N)
+            // Space complexity: O(1)
+        if(n==0) return 0;
+        int prev = 1;
+        int prev2 = 0;
+
+        for(int i=2;i<=n;i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 }
