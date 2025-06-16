edb8b1bd0c1a1ccff06e92d5d003f96346a03af8
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-16 20:32:46 +0530

    Updated

--- DP/fibonacci.java
+++ DP/fibonacci.java
@@ -7,9 +7,9 @@ public class fibonacci{
             System.out.println("Enter number greater than 0");
             return;
         }
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n;i++){
             dp[i] = -1;
         }
         System.out.println("Fibonacci Sum of "+n+": "+FibCal(n,dp));
@@ -18,7 +18,7 @@ public class fibonacci{
     public static int FibCal(int n, int[] dp){
         if(n<=1) return n;
 
-        if(dp[n-1] != -1) return dp[n-1];
+        if(dp[n] != -1) return dp[n];
 
         return FibCal(n-1, dp)+FibCal(n-2, dp);
     }
