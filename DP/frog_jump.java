c8461cdabba5dced261299240565dcceb4ccda7d
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-17 17:32:32 +0530

    memoization technique

--- DP/frog_jump.java
+++ DP/frog_jump.java
@@ -2,9 +2,12 @@ package DP;
 
 public class frog_jump {
     public static void main(String[] args) {
-        int[] height = {10, 30, 40, 20};
+        int[] height = {30, 20, 50, 10, 40};
         int result = minCost(height);
-        System.out.println("Minimum cost: " + result);
+        System.out.println("Minimum cost using tabulation: " + result);
+
+        result = minCost2(height);
+        System.out.println("Minimum cost using memoization: " + result);
     }
 
     public static int minCost(int[] height){
@@ -20,5 +23,21 @@ public class frog_jump {
             dp[i] = Math.min(onejump,twojump);
         }
         return dp[n-1];
+    }
+
+    public static int minCost2(int[] height){
+        int n = height.length;
+        int[] dp = new int[n+1];
+        return memoize(n-1,height,dp);
+    }
+
+    private static int memoize(int n, int[] height, int[] dp) {
+        if(n==0) return 0;
+
+        int onejump = memoize(n-1, height, dp) + Math.abs(height[n] - height[n-1]);
+        int twojump = Integer.MAX_VALUE;
+        if(n>1) twojump = memoize(n-2, height, dp) + Math.abs(height[n] - height[n-2]);
+
+        return dp[n] = Math.min(onejump, twojump);
     }
 }
