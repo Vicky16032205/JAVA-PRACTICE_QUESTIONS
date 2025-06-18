e76a8a96f89ba25aba4e161903785205a6e928ca
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-18 12:53:03 +0530

    finding maximum sum using memoization

--- DP/max_non_adj_sum.java
+++ DP/max_non_adj_sum.java
@@ -5,8 +5,8 @@ public class max_non_adj_sum {
         int[] arr = {5, 5, 10, 100, 10, 5};
         System.out.println("Recursion answer: " + findMaxSum(arr,arr.length-1));
 
-        // int[] dp = new int[arr.length+1];
-        // System.out.println("Memoization answer: " + MemoFindMaxSum(arr,arr.length-1,dp));
+        int[] dp = new int[arr.length+1];
+        System.out.println("Memoization answer: " + MemoFindMaxSum(arr,arr.length-1,dp));
 
         // int[] dp2 = new int[arr.length];
         // System.out.println("Memoization answer: " + TabuFindMaxSum(arr,arr.length-1,dp2));
@@ -22,16 +22,16 @@ public class max_non_adj_sum {
         return Math.max(pick, non_pick);
     }
 
-    // public static int MemoFindMaxSum(int[] arr, int n, int[] dp){
-    //     if(n==0) return arr[0];
-    //     if(n<1) return 0;
-    //     if(dp[n] != 0) return dp[n];
+    public static int MemoFindMaxSum(int[] arr, int n, int[] dp){
+        if(n==0) return arr[0];
+        if(n<1) return 0;
+        if(dp[n] != 0) return dp[n];
 
-    //     int pick = arr[n] + MemoFindMaxSum(arr, n-2, dp);
-    //     int non_pick = 0 + MemoFindMaxSum(arr, n-1, dp);
+        int pick = arr[n] + MemoFindMaxSum(arr, n-2, dp);
+        int non_pick = 0 + MemoFindMaxSum(arr, n-1, dp);
 
-    //     return dp[n] = Math.max(pick, non_pick);
-    // }
+        return dp[n] = Math.max(pick, non_pick);
+    }
 
     // public static int TabuFindMaxSum(int[] arr, int n, int[] dp){
         
