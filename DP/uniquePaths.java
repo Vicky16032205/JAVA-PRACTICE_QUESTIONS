ec0f8558fd34dc9ffbb323750d7e14dca593c819
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-19 14:37:02 +0530

    Unique paths problem using memoization

--- DP/uniquePaths.java
+++ DP/uniquePaths.java
@@ -6,22 +6,22 @@ public class uniquePaths {
         int col = 6;
         System.out.println("Recursive method answer: " +totalWays(1,1,row,col));
 
-        // int[][] dp = new int[row+1][col+1];
-        // System.out.println("Memoization method answer: " +countWays(1,1,row,col,dp));
+        int[][] dp = new int[row+1][col+1];
+        System.out.println("Memoization method answer: " +countWays(1,1,row,col,dp));
     }
 
-    // private static int countWays(int sr, int sc, int er, int ec, int[][] dp) {
-    //     if(sr < 1 || sc < 1 || sr>er || sc>ec) return 0;
+    private static int countWays(int sr, int sc, int er, int ec, int[][] dp) {
+        if(sr < 1 || sc < 1 || sr>er || sc>ec) return 0;
 
-    //     if(sr == er && sc == ec) return 1;
+        if(sr == er && sc == ec) return 1;
 
-    //     if(dp[sr][sc] != 0) return dp[sr][sc];
+        if(dp[sr][sc] != 0) return dp[sr][sc];
 
-    //     int downways = totalWays(sr+1 , sc , er, ec);
-    //     int rightways = totalWays(sr , sc+1 , er, ec);
+        int downways = totalWays(sr+1 , sc , er, ec);
+        int rightways = totalWays(sr , sc+1 , er, ec);
 
-    //     return dp[sr][sc] = downways + rightways;
-    // }
+        return dp[sr][sc] = downways + rightways;
+    }
 
     private static int totalWays(int sr, int sc, int er, int ec) {
         if(sr == er && sc == ec){
