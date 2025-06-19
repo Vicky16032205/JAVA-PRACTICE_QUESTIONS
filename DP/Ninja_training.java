10b1a3998bc371fcc97e7eff7be3a95ab9777707
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-19 12:44:44 +0530

    Tabulation method for Ninja's training problem

--- DP/Ninja_training.java
+++ DP/Ninja_training.java
@@ -9,6 +9,31 @@ public class Ninja_training {
                     };
         int[][] dp = new int[arr.length][4];
         System.out.println(maximumPoints(arr,arr.length-1,3,dp));
+
+        int[][] dp2 = new int[arr.length][4];
+        System.out.println(maximumPointsTabu(arr,arr.length,dp2));
+    }
+
+    private static int maximumPointsTabu(int[][] arr, int n, int[][] dp) {
+        dp[0][0] = Math.max(arr[0][1] , arr[0][2]);
+        dp[0][1] = Math.max(arr[0][0] , arr[0][2]);
+        dp[0][2] = Math.max(arr[0][1] , arr[0][0]);
+        dp[0][3] = Math.max(arr[0][0] , Math.max(arr[0][1] , arr[0][2]));
+        
+        for(int day=1;day<n;day++){
+            for(int last = 0;last<4;last++){
+                
+                dp[day][last] = 0;
+                
+                for(int task = 0;task < 3;task++){
+                    if(task != last){
+                        int point = arr[day][task] + dp[day-1][task];
+                        dp[day][last] = Math.max(dp[day][last] , point);
+                    }
+                }
+            }
+        }
+        return dp[n-1][3];
     }
 
     private static int maximumPoints(int[][] arr, int n, int done, int[][] dp) {
