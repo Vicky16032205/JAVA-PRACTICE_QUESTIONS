c6a695ba8a37089c6d8dc2a46e09477efe8539b9
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-22 13:17:36 +0530

    Memoization  solution for cherry pickup ii problem

--- DP/cherryPickup.java
+++ DP/cherryPickup.java
@@ -11,43 +11,43 @@ public class cherryPickup {
             {2, 1, 1}
         };
         System.out.println("Recursive Answer: " +cherryPickup2(grid));
-        // System.out.println("Recursive Answer: " +cherryPickup3(grid));
+        System.out.println("Recursive Answer: " +cherryPickup3(grid));
     }
 
-    // public static int cherryPickup3(int[][] grid){
-    //     int r = grid.length;
-    //     int c = grid[0].length;
-    //     int[][][] dp = new int[r][c][c];
-    //     for(int[][] row : dp){
-    //         for (int[] col : row){
-    //             Arrays.fill(col, -1);
-    //         }
-    //     }
-    //     return f2(0,0,c-1,r,c,grid, dp);
-    // }
+    public static int cherryPickup3(int[][] grid){
+        int r = grid.length;
+        int c = grid[0].length;
+        int[][][] dp = new int[r][c][c];
+        for(int[][] row : dp){
+            for (int[] col : row){
+                Arrays.fill(col, -1);
+            }
+        }
+        return f2(0,0,c-1,r,c,grid, dp);
+    }
 
-    // public static int f2(int i, int j1, int j2, int r, int c, int[][] grid, int[][][] dp){
-    //     if(j1<0 || j2<0 || j1>=c || j2>=c) return 0;
+    public static int f2(int i, int j1, int j2, int r, int c, int[][] grid, int[][][] dp){
+        if(j1<0 || j2<0 || j1>=c || j2>=c) return 0;
 
-    //     if(i == r-1){
-    //         if(j1 == j2) return grid[i][j1];
-    //         else return grid[i][j1] + grid[i][j2] ;
-    //     }
-    //     if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
-    //     int maxi = 0;
+        if(i == r-1){
+            if(j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2] ;
+        }
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        int maxi = 0;
 
-    //     for(int dj1=-1;dj1<=1;dj1++){
-    //         for(int dj2=-1;dj2<=1;dj2++){
-    //             int val = f2(i+1, j1+dj1, j2+dj2, r, c, grid, dp);
-    //             if(j1 == j2) val += grid[i][j1];
-    //             else val += grid[i][j1] + grid[i][j2];
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                int val = f2(i+1, j1+dj1, j2+dj2, r, c, grid, dp);
+                if(j1 == j2) val += grid[i][j1];
+                else val += grid[i][j1] + grid[i][j2];
 
-    //             maxi = Math.max(val, maxi);
-    //             dp[i][j1][j2] = maxi;
-    //         }
-    //     }
-    //     return maxi;
-    // }
+                maxi = Math.max(val, maxi);
+                dp[i][j1][j2] = maxi;
+            }
+        }
+        return maxi;
+    }
 
     public static int cherryPickup2(int[][] grid){
         int r = grid.length;
