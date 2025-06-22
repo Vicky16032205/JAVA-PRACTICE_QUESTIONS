8f5066c68d5d49859ae69705756401e17478cadf
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-22 14:44:25 +0530

    Memoization of subset problem

--- DP/subsetSumEqualsTarget.java
+++ DP/subsetSumEqualsTarget.java
@@ -4,7 +4,26 @@ public class subsetSumEqualsTarget {
     public static void main(String[] args) {
         int[] arr = {2,3,1,1};
         int target = 4;
-        System.out.println(targetPresent(arr,arr.length-1,target));
+        System.out.println("Recursive solution: " +targetPresent(arr,arr.length-1,target));
+
+        boolean[] dp = new boolean[arr.length];
+        System.out.println("Memoization solution: " +target(arr,arr.length-1,target,dp));
+    }
+
+    private static boolean target(int[] arr, int n, int target, boolean[] dp) {
+        if(n==0){
+            return arr[0] == target;
+        }
+        if(target == 0) return true;
+
+        if(dp[n] != false) return true;
+
+        boolean not_take = targetPresent(arr, n-1, target);
+        boolean take = false;
+        if(arr[n] <= target){
+            take = targetPresent(arr, n-1, target-arr[n]);
+        }
+        return dp[n] = not_take || take;
     }
 
     private static boolean targetPresent(int[] arr, int n, int target) {
