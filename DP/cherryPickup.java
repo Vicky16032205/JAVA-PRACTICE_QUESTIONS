75e811910c7d73ed94d159ffd4e76233341d5853
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-22 14:29:55 +0530

    Tabulation method for cherry pickup ii problem

--- DP/cherryPickup.java
+++ DP/cherryPickup.java
@@ -11,7 +11,49 @@ public class cherryPickup {
             {2, 1, 1}
         };
         System.out.println("Recursive Answer: " +cherryPickup2(grid));
-        System.out.println("Recursive Answer: " +cherryPickup3(grid));
+        System.out.println("Memoization Answer: " +cherryPickup3(grid));
+        System.out.println("Tabulation Answer: " +cherryPickup4(grid));
+    }
+
+    public static int cherryPickup4(int[][] grid){
+        int r = grid.length;
+        int c = grid[0].length;
+        int[][][] dp = new int[r][c][c];
+
+        return f3(r,c,grid, dp);
+    }
+
+    public static int f3(int n, int m, int[][] grid, int[][][] dp){
+        for(int j1 = 0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j1 = 0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+
+                    int maxi = 0;
+                    for(int dj1=-1;dj1<=1;dj1++){
+                        for(int dj2=-1;dj2<=1;dj2++){
+
+                            int val = 0;
+                            if(j1 == j2) val = grid[i][j1];
+                            else val = grid[i][j1] + grid[i][j2];
+
+                            if(j1+dj1 >=0 && j2+dj2 >=0 && j1+dj1 <m && j2+dj2 <m){
+                                val += dp[i+1][j1+dj1][j2+dj2];
+                            }
+                            maxi = Math.max(maxi, val);
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 
     public static int cherryPickup3(int[][] grid){
