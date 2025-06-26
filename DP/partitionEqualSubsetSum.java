c2ab7318404a2e7de5da9947e33724b86c377b35
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-26 14:20:49 +0530

    Memoization solution for partition Equal subset sum problem

--- DP/partitionEqualSubsetSum.java
+++ DP/partitionEqualSubsetSum.java
@@ -4,7 +4,7 @@ public class partitionEqualSubsetSum {
     public static void main(String[] args) {
         int[] arr = {2,3,3,3,4,7};
         System.out.println("Recursive solution: " + sumPossible1(arr,arr.length));
-        // System.out.println("Memoization solution: " + sumPossible2(arr,arr.length));
+        System.out.println("Memoization solution: " + sumPossible2(arr,arr.length));
         // System.out.println("Tabulation solution: " + sumPossible3(arr,arr.length));
     }
 
@@ -44,29 +44,29 @@ public class partitionEqualSubsetSum {
         return partitionPossible(totalSum/2,arr,n-1);
     }
 
-    // public static boolean sumPossible2(int[] arr, int n){
-    //     int totalSum = 0;
-    //     for(int i=0;i<n;i++){
-    //         totalSum += arr[i];
-    //     }
-    //     if(totalSum % 2 != 0) return false;
-    //     Boolean[][] dp = new Boolean[n+1][totalSum/2+1];
-    //     return memoize(dp, totalSum/2, arr, n-1);
-    // }
+    public static boolean sumPossible2(int[] arr, int n){
+        int totalSum = 0;
+        for(int i=0;i<n;i++){
+            totalSum += arr[i];
+        }
+        if(totalSum % 2 != 0) return false;
+        Boolean[][] dp = new Boolean[n+1][totalSum/2+1];
+        return memoize(dp, totalSum/2, arr, n-1);
+    }
 
-    // private static boolean memoize(Boolean[][] dp, int k, int[] arr, int n) {
-    //     if(k==0) return true;
-    //     if(n == 0) return arr[0] == k;
-    //     if(n<0) return false;
+    private static boolean memoize(Boolean[][] dp, int k, int[] arr, int n) {
+        if(k==0) return true;
+        if(n == 0) return arr[0] == k;
+        if(n<0) return false;
         
-    //     if(dp[n][k] != null) return dp[n][k];
+        if(dp[n][k] != null) return dp[n][k];
 
-    //     boolean not_take = memoize(dp, k, arr, n-1);
-    //     boolean take = false;
-    //     if(arr[n] <= k) take = memoize(dp, k-arr[n], arr, n-1);
+        boolean not_take = memoize(dp, k, arr, n-1);
+        boolean take = false;
+        if(arr[n] <= k) take = memoize(dp, k-arr[n], arr, n-1);
 
-    //     return dp[n][k] = take || not_take;
-    // }
+        return dp[n][k] = take || not_take;
+    }
 
     public static boolean partitionPossible(int k , int[] arr, int n){
         if(k == 0) return true;
