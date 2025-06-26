dcf4f4fe33ba3e5878ff38002b4d7b15262f7d8f
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-26 12:46:46 +0530

    Tabulation way for subset sum problem

--- DP/subsetSumEqualsTarget.java
+++ DP/subsetSumEqualsTarget.java
@@ -8,6 +8,28 @@ public class subsetSumEqualsTarget {
 
         Boolean[][] dp = new Boolean[arr.length][target+1];
         System.out.println("Memoization solution: " +target(arr,arr.length-1,target,dp));
+
+        boolean[][] dp2 = new boolean[arr.length][target+1];
+        System.out.println("Tabulation solution: " +Present(arr,arr.length,target,dp2));
+    }
+
+    private static boolean Present(int[] arr, int n, int target, boolean[][] dp) {
+        for(int i=0;i<n;i++) dp[i][0] = true;
+        
+        dp[0][arr[0]] = true;
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=target;j++){
+                boolean not_take = dp[i-1][j];
+                boolean take = false;
+
+                if(j >= arr[i]){
+                    take = dp[i-1][j-arr[i]];
+                }
+                dp[i][j] = take || not_take;
+            }
+        }
+        return dp[n-1][target];
     }
 
     private static boolean target(int[] arr, int n, int target, Boolean[][] dp) {
