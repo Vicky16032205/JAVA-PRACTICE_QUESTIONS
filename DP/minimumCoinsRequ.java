3a3a0417427ad00db36b293a67f1f3b6e54ee121
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-01 23:44:46 +0530

    Tabulation method

--- DP/minimumCoinsRequ.java
+++ DP/minimumCoinsRequ.java
@@ -4,14 +4,27 @@ import java.util.* ;
 public class minimumCoinsRequ {
 
     public static int minimumCoinsTabu(int p, int[] arr){
-        int[][] dp = new int[arr.length-1][p+1];
+        int[][] dp = new int[arr.length][p+1];
 
-        return minCoinsTabulation(p,arr,arr.length-1, dp);
+        return minCoinsTabulation(p,arr,arr.length, dp);
     }
 
     public static int minCoinsTabulation(int p, int[] arr, int n, int[][] dp){
+        for(int i=0;i<=p;i++){
+            if(i%arr[0] == 0) dp[0][i] = i/arr[0];
+            else dp[0][i] = Integer.MAX_VALUE;
+        }
 
-        return 0;
+        for(int i=1;i<n;i++){
+            for(int T=0;T<=p;T++){
+                int not_pick = 0 + dp[i-1][T];
+                int pick = (Integer.MAX_VALUE);
+                if(arr[i] <= T) pick = 1 + dp[i][T-arr[i]];
+
+                dp[i][T] = Math.min(pick , not_pick);
+            }
+        }
+        return dp[n-1][p];
     }
 
 
@@ -39,7 +52,7 @@ public class minimumCoinsRequ {
         int[] arr = {1,2,3};
         int changes = 8;
         System.out.println(minimumCoins(changes,arr));
-        // System.out.println(minimumCoinsTabu(changes,arr));
+        System.out.println(minimumCoinsTabu(changes,arr));
     }
 }
 
