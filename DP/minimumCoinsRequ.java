041752601613df221979cdb4db31159017ea8d88
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-01 23:22:40 +0530

    Small change in code

--- DP/minimumCoinsRequ.java
+++ DP/minimumCoinsRequ.java
@@ -2,6 +2,19 @@ package DP;
 import java.util.* ;
 
 public class minimumCoinsRequ {
+
+    public static int minimumCoinsTabu(int p, int[] arr){
+        int[][] dp = new int[arr.length-1][p+1];
+
+        return minCoinsTabulation(p,arr,arr.length-1, dp);
+    }
+
+    public static int minCoinsTabulation(int p, int[] arr, int n, int[][] dp){
+
+        return 0;
+    }
+
+
     public static int minimumCoins(int p, int[] arr){
         int[][] dp = new int[arr.length][p+1];
         for(int[] row : dp) Arrays.fill(row , -1);
@@ -10,11 +23,14 @@ public class minimumCoinsRequ {
     }
 
     public static int minCoins(int p, int[] arr, int n, int[][] dp){
-        if(n==0) return p;
+        if(n==0){
+            if(p % arr[0] == 0) return p/arr[0];
+            else return Integer.MAX_VALUE;
+        }
         if(dp[n][p] != -1) return dp[n][p];
         int not_pick = 0 + minCoins(p,arr,n-1,dp);
         int pick = (Integer.MAX_VALUE-100);
-        if(arr[n] <= p) pick = 1 + minCoins((p-arr[n]) , arr, n,dp);
+        if(arr[n] <= p) pick = 1 + minCoins((p-arr[n]) , arr, n, dp);
 
         return dp[n][p] = Math.min(not_pick , pick);
     }
@@ -23,6 +39,7 @@ public class minimumCoinsRequ {
         int[] arr = {1,2,3};
         int changes = 8;
         System.out.println(minimumCoins(changes,arr));
+        // System.out.println(minimumCoinsTabu(changes,arr));
     }
 }
 
