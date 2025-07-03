f7933f34d170dc1ab5f2d4e82590bbd46b978a52
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-03 18:40:07 +0530

    Optimized space for longest common subsequence problem

--- DP/LongestCommonSubsequence.java
+++ DP/LongestCommonSubsequence.java
@@ -14,6 +14,26 @@ public class LongestCommonSubsequence {
 
         int[][] dp2 = new int[str1.length()+1][str2.length()+1];
         System.out.println("Tabulation solution: " + tabulation(str1.length() , str2.length() , str1 , str2, dp2));
+
+        System.out.println("Optimized space solution: " + space(str1.length() , str2.length() , str1 , str2));
+    }
+
+    public static int space(int idx1 , int idx2, String str1 , String str2){
+        int[] prev = new int[str2.length()+1];
+
+        for(int i=0;i<=str2.length();i++){
+            prev[i] = 0;
+        }
+
+        for(int i=1;i<=str1.length();i++){
+            int[] curr = new int[str2.length()+1];
+            for(int j=1;j<=str2.length();j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)) curr[j] = 1 + prev[j-1];
+                else curr[j] = Math.max(prev[j] , curr[j-1]);
+            }
+            prev = curr;
+        }
+        return prev[str2.length()];
     }
 
     public static int tabulation(int idx1 , int idx2, String str1 , String str2, int[][] dp){
