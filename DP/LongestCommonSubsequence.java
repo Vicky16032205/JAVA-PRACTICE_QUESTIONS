3a6cd05fdae1d36715ba240a76b2529e2551d8a8
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-03 18:06:50 +0530

    Memoization of longest common substring

--- DP/LongestCommonSubsequence.java
+++ DP/LongestCommonSubsequence.java
@@ -1,14 +1,24 @@
 package DP;
 
+import java.util.Arrays;
+
 public class LongestCommonSubsequence {
     public static void main(String[] args) {
-        String str1 = "acd";
-        String str2 = "ced";
-        System.out.println(printCommon(str1,str2));
+        String str1 = "abcde";
+        String str2 = "abcdef";
+        System.out.println("Recursive solution: " + recursive(str1.length()-1 , str2.length()-1 , str1 , str2));
+
+        int[][] dp = new int[str1.length()+1][str2.length()+1];
+        for(int[] row: dp) Arrays.fill(row, -1);
+        System.out.println("Memoization solution: " + memoize(str1.length()-1 , str2.length()-1 , str1 , str2, dp));
     }
 
-    public static int printCommon(String str1 , String str2){
-        return recursive(str1.length()-1 , str2.length()-1 , str1 , str2);
+    public static int memoize(int idx1 , int idx2, String str1 , String str2, int[][] dp){
+        if(idx1 <0 || idx2 < 0) return 0;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+
+        if(str1.charAt(idx1) == str2.charAt(idx2)) return dp[idx1][idx2] = 1 + memoize(idx1-1, idx2-1, str1, str2, dp);
+        return dp[idx1][idx2] = Math.max(memoize(idx1-1, idx2, str1, str2, dp) , memoize(idx1, idx2-1, str1, str2, dp));
     }
 
     public static int recursive(int idx1 , int idx2, String str1 , String str2){
