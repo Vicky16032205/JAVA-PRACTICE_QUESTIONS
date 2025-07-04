2eb28ac53a2f2ca3aef7b5688184f18df7a5b790
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-04 16:29:11 +0530

    Tabulation method implemented for longest common substring problem

--- DP/longestCommonSubstring.java
+++ DP/longestCommonSubstring.java
@@ -3,12 +3,36 @@ package DP;
 public class longestCommonSubstring {
     static int ans =0;
     public static void main(String[] args) {
-        String s = "abcde";
-        String t = "cdegabcde";
+        String s = "abcd";
+        String t = "abzd";
 
         System.out.println("Recursive answer " + common(s.length()-1 , t.length()-1 , s , t , 0));
         int[][] dp = new int[s.length()+1][t.length()+1];
         System.out.println("Memoization answer " + memoize(s.length()-1 , t.length()-1 , s , t , 0, dp));
+
+        int[][] dp2 = new int[s.length()+1][t.length()+1];
+        System.out.println("Tabulation answer " + tabulation(s.length() , t.length() , s , t ,dp2));
+    }
+
+    public static int tabulation(int idx1, int idx2,  String s, String t, int[][] dp){
+        for(int j=0;j<=idx2;j++){
+            dp[0][j] = 0;
+        }
+        for(int i=0;i<=idx1;i++){
+            dp[i][0] = 0;
+        }
+
+        int res = 0;
+        for(int i=1;i<=idx1;i++){
+            for(int j=1;j<=idx2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    res = Math.max(res,dp[i][j]);
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        return res;
     }
 
     public static int memoize(int idx1, int idx2,  String s, String t, int count , int[][] dp){
