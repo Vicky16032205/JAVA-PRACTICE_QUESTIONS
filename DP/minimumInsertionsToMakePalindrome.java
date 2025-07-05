445786229a955333db79438e8c279dd223e5152d
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-05 13:35:53 +0530

    Minimum Insertions to make string palindrome using tabulation

--- DP/minimumInsertionsToMakePalindrome.java
+++ DP/minimumInsertionsToMakePalindrome.java
@@ -13,7 +13,26 @@ public class minimumInsertionsToMakePalindrome {
        int[][] dp = new int[s.length()+1][t.length()+1];
        for(int[] row : dp) Arrays.fill(row, -1);
        int ans1 = memoization(s.length()-1 , t.length()-1 , s , t , dp);
-       System.out.println("Minimum Insertions required using tabulation = "+ (s.length()- ans1));
+       System.out.println("Minimum Insertions required using memoization = "+ (s.length()- ans1));
+
+       int[][] dp2 = new int[s.length()+1][t.length()+1];
+       int ans2 = tabulation(s.length() , t.length() , s , t , dp2);
+       System.out.println("Minimum Insertions required using tabulation = "+ (s.length()- ans2));
+    }
+
+    public static int tabulation(int idx1, int idx2 , String s  , String t , int[][] dp){
+        for(int i=0;i<=idx1;i++){
+            dp[i][0] = 0;
+            dp[0][i] = 0;
+        }
+
+        for(int i=1;i<=idx1;i++){
+            for(int j=1;j<=idx2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)) dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[s.length()][t.length()];
     }
 
     public static int memoization(int idx1, int idx2 , String s  , String t , int[][] dp){
