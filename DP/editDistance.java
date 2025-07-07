8400de3769794a91b1767fdb9193d1b3f0f77b04
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-07 15:32:20 +0530

    edit distance done using tabulation

--- DP/editDistance.java
+++ DP/editDistance.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class editDistance {
     public static void main(String[] args) {
-        String s = "horse";
-        String t = "ros";
+        String s = "intention";
+        String t = "execution";
 
         System.out.println("minimum operations required to convert string (s) to string (t) is : ==>");
         System.out.println("Using recursion: "+recursion(s.length()-1, t.length()-1 , s, t));
@@ -13,9 +13,38 @@ public class editDistance {
         int[][] dp = new int[s.length()+1][t.length()+1];
         for(int[] row : dp) Arrays.fill(row, -1);
         System.out.println("Using memoization: "+ memoization(s.length()-1, t.length()-1 , s, t , dp));
+
+        int[][] dp2 = new int[s.length()+1][t.length()+1];
+        System.out.println("Using tabulation: "+ tabulation(s.length(), t.length() , s, t , dp2));
     }
 
-        public static int memoization(int idx1 , int idx2 , String s , String t , int[][] dp){
+    public static int tabulation(int idx1 , int idx2 , String s , String t , int[][] dp){
+        for(int i=0;i<=idx1;i++){
+            dp[i][0] = i;
+        }
+
+        for(int j=0;j<=idx2;j++){
+            dp[0][j] = j;
+        }
+
+        for(int i=1;i<=idx1;i++){
+            for(int j=1;j<=idx2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    int insert = dp[i-1][j];
+                    int delete = dp[i][j-1];
+                    int replace = dp[i-1][j-1];
+
+                    dp[i][j] = 1 + Math.min(insert , Math.min(delete , replace));
+                }
+            }
+        }
+        return dp[idx1][idx2];
+    }
+
+    public static int memoization(int idx1 , int idx2 , String s , String t , int[][] dp){
         if(idx1<0) return idx2+1;
         if(idx2<0) return idx1+1;
         if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
