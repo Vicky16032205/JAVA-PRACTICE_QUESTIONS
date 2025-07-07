2cbdf5247ed026e29ac2692cdd8af16e0f6add5f
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-07 12:45:40 +0530

    Distinct subsequences of a string in another string by optimizing space

--- DP/distinctSubsequences.java
+++ DP/distinctSubsequences.java
@@ -4,26 +4,44 @@ import java.util.Arrays;
 
 public class distinctSubsequences {
     public static void main(String[] args) {
-        String s = "babgbag";
-        String t = "bag";
+        String s = "rabbbit";
+        String t = "rabbit";
+
+        System.out.println("Total recursive matching: "+ recursion(s.length(), t.length(),s,t));
 
         int[][] dp = new int[s.length()+1][t.length()+1];
         for(int[] row : dp) Arrays.fill(row, -1);
-        System.out.println("Total recursive matching: "+ recursion(s.length(), t.length(),s,t));
         System.out.println("Total recursive matching using memoization: "+ memoization(s.length(), t.length(),s,t, dp));
 
         int[][] dp2 = new int[s.length()+1][t.length()+1];
-        System.out.println("Total recursive matching using tabulation: "+ tabulation(s.length(), t.length(),s,t, dp2));
+        System.out.println("Total iterative matching using tabulation: "+ tabulation(s.length(), t.length(),s,t, dp2));
+
+        System.out.println("Total matching by optimizing space: "+ spaceOptimization(s.length(), t.length(),s,t));
+    }
+
+    public static int spaceOptimization(int idx1 ,int idx2 ,String s ,String t){
+        int[] prev = new int[idx2+1];
+
+        prev[0] = 1;
+
+        for(int i=1;i<=idx1;i++){
+            int[] curr = new int[idx2+1];
+            curr[0] = 1;
+            for(int j=1;j<=idx2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    curr[j] = prev[j-1] + prev[j];
+                }
+                else curr[j] = prev[j];
+            }
+            prev = curr;
+        }
+        return prev[idx2];
     }
 
     public static int tabulation(int idx1 ,int idx2 ,String s ,String t, int[][] dp){
         // change base case to iterations and find its value
         for(int i=0;i<=idx1;i++){
             dp[i][0] = 1;         // not matter on what index of string1, value of size of string2 becomes 0 will give output as 1;
-        }
-
-        for(int j=1;j<=idx2;j++){
-            dp[0][j] = 0;           // if at any index of string2, if the value of size of index1 becomes 0 will give output as 0; 
         }
 
         for(int i=1;i<=idx1;i++){
