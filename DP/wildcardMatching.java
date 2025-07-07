bdea0a596759c43d96b33e5b816b65a3c8a38e05
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-07 23:29:03 +0530

    Tabulation method for wild card matching problem

--- DP/wildcardMatching.java
+++ DP/wildcardMatching.java
@@ -2,20 +2,48 @@ package DP;
 
 public class wildcardMatching {
     public static void main(String[] args) {
-        String s = "m??*ss*?i*pi";
-        String t = "mississippi";
+        String s = "*a*b";
+        String t = "adceb";
 
         System.out.println("Recursive answer: " + possible(s.length()-1 , t.length()-1 , s, t));
 
         Boolean[][] dp = new Boolean[s.length()+1][t.length()+1];
         System.out.println("Memoization answer: " + memoization(s.length()-1 , t.length()-1 , s, t, dp));
+
+        boolean[][] dp2 = new boolean[s.length()+1][t.length()+1];
+        System.out.println("Tabulation answer: " + tabulation(s.length() , t.length() , s, t, dp2));
+    }
+
+    public static boolean tabulation(int idx1 ,int idx2 ,String s, String t, boolean[][] dp){
+        dp[0][0] = true;
+
+        for(int i=1;i<=idx1;i++){
+            boolean flag = true;
+            for(int ii=1;ii<=i;ii++){
+                if(s.charAt(ii-1) != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+
+        for(int i=1;i<=idx1;i++){
+            for(int j=1;j<=idx2;j++){
+                if(s.charAt(i-1) == t.charAt(j-1) || s.charAt(i-1) == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if(s.charAt(i-1) == '*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[idx1][idx2];
     }
 
     public static boolean memoization(int idx1, int idx2, String s , String t, Boolean[][] dp){
         if(idx1 < 0 && idx2 < 0) return true;
-        if(idx1 < 0){
-            return false;
-        }
+        if(idx1 < 0) return false;
         if(idx2 < 0){
             for(int i=0;i<=idx1;i++){
                 if(s.charAt(i) != '*') return false;
