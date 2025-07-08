f1687a9f19d66965ba5424f94d32d41f6b5e7a83
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-08 12:08:59 +0530

    Buy and Sell stocks III problem using memoization

--- DP/buyAndSellStocksIII.java
+++ DP/buyAndSellStocksIII.java
@@ -1,13 +1,42 @@
 package DP;
 
+import java.util.Arrays;
+
 class buyAndSellStocksIII {
     public static void main(String[] args) {
-        int[] prices = {7,6,4,3,1};
-        int transactions = 2;
+        int[] prices = {1,2,3,4,5};
 
-        System.out.println("Buy and Sell stocks II problem of leetcode ways to solve the problem ==>");
-        System.out.println("Using recursion: "+profitMade(0,1,prices,transactions));
+        System.out.println("Buy and Sell stocks III problem of leetcode ways to solve the problem ==>");
+        System.out.println("Using recursion: "+profitMade(0,1,prices,2));
+
+        int[][][] dp = new int[prices.length+1][2][2];
+        for(int[][] row : dp){
+            for(int[] rows: row){
+                Arrays.fill(rows, -1);
+            }
+        }
+        System.out.println("Using memoization: "+memoization(0,1,prices,1, dp));
     }
+
+    
+
+    private static int memoization(int idx, int buy, int[] prices, int transactions, int[][][] dp) {
+        if(transactions < 0) return 0;
+        if(idx == prices.length) return 0;
+        if(dp[idx][buy][transactions] != -1) return dp[idx][buy][transactions];
+        
+        int profit = 0;
+        if(buy==1){
+            profit = Math.max((-prices[idx] + memoization(idx+1, 0, prices, transactions, dp)) , memoization(idx+1, 1, prices, transactions, dp));
+        }
+        else{
+            profit = Math.max((prices[idx] + memoization(idx+1, 1, prices, transactions-1 , dp)) , memoization(idx+1, 0, prices, transactions, dp));
+        }
+
+        return dp[idx][buy][transactions] = profit;
+    }
+
+
 
     private static int profitMade(int idx, int buy, int[] prices, int transactions) {
         if(transactions == 0) return 0;
