b445998a7de7aff3197fd6bdac700202d3eeb2c4
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-08 10:57:01 +0530

    buy and sell stocks II done using tabulation

--- DP/buyAndSellStockesII.java
+++ DP/buyAndSellStockesII.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class buyAndSellStockesII {
     public static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(maxProfit(prices));
-    }
-
-    public static int maxProfit(int[] prices) {
+        int[] prices = {7,6,4,3,1};
+        System.out.println("Buy and Sell stocks II problem of leetcode ways to solve the problem ==>");
         int[][] dp = new int[prices.length+1][2];
         for(int[] row: dp) Arrays.fill(row,-1);
-        return profitMade(0,1,prices, dp);
+        System.out.println("Using memoization: "+profitMade(0,1,prices, dp));
+
+        int[][] dp2 = new int[prices.length+1][2];
+        System.out.println("Using Tabulation: "+tabulation(prices, dp2));
     }
 
     public static int profitMade(int day , int buy , int[] prices, int[][] dp){
@@ -27,5 +27,25 @@ public class buyAndSellStockesII {
         }
 
         return dp[day][buy] = profit;
+    }
+
+    public static int tabulation(int[] prices, int[][] dp){
+        int n = prices.length;
+        dp[n][0] = dp[n][1] = 0;
+
+        for(int idx= n-1;idx>=0;idx--){
+            for(int buy =0;buy<=1;buy++){
+                int profit = 0;
+                if(buy==1){
+                    profit = Math.max((-prices[idx] + dp[idx+1][0]) , dp[idx+1][1]);
+                }
+                else{
+                    profit = Math.max((prices[idx] + dp[idx+1][1]) , dp[idx+1][0]);
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+
+        return dp[0][1];
     }
 }
