7dd10def0135b6adeb20ed618d2e07d80934e929
vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-10 11:43:30 +0530

    Element finding for LIS

--- DP/LIS_1D_array.java
+++ DP/LIS_1D_array.java
@@ -9,6 +9,39 @@ public class LIS_1D_array {
         Arrays.fill(dp,1);
         
         System.out.println(Nsq(nums, dp));
+        int[] dp2 = new int[nums.length];
+        Arrays.fill(dp2,1);
+        printLIS(nums, dp2);
+    }
+
+    public static void printLIS(int[] nums, int[] dp){
+        int maxi = 1;
+        int idx =-1;
+        int[] hash = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i] > nums[j] && dp[i] < 1+dp[j]){
+                    dp[i] = 1+dp[j];
+                    hash[i] = j;
+                    maxi = Math.max(maxi , dp[i]);
+                }
+            }
+        }
+
+        for(int i=0;i<hash.length;i++){
+            if(idx<hash[i]) idx = i;
+        }
+
+        int[] ans = new int[maxi];
+        for(int i=0;i<maxi;i++){
+            ans[i] = nums[idx];
+            idx = hash[idx];
+        }
+
+        for(int i=ans.length-1;i>=0;i--){
+            System.out.print(ans[i]+" ");
+        }
     }
 
     public static int Nsq(int[] nums , int[] dp){
