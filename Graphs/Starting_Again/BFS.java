e85c2df26f9148cc3ed14eaf369a248a48888d85
Vicky16032205 <vickyguptagkp55@gmail.com>
2025-07-27 17:09:24 +0530

    Add BFS implementation with graph creation and traversal

---
+++ Graphs/Starting_Again/BFS.java
@@ -0,0 +1,74 @@
+package Graphs.Starting_Again;
+
+import java.util.*;
+
+public class BFS {
+
+    public static class Edge{
+        int src;
+        int dest;
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    private static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6, 5));
+    }
+
+    private static void bfs(ArrayList<Edge>[] graph, boolean[] vis, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while(!q.isEmpty()){
+            int val = q.remove();
+            if(vis[val] == true) return;
+            System.out.print(val+ " ");
+            vis[val] = true;
+
+            for(int i=0;i<graph[val].size();i++){
+                Edge e = graph[val].get(i);
+                q.add(e.dest);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+
+        boolean[] vis = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(vis[i] == false){
+                bfs(graph, vis, i);
+            } 
+        }
+    }
+}
